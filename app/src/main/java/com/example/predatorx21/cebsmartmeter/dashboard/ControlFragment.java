c8799e9973643f1f67229190afdf6cda3d8c66f3
Avishka Dhanajaya <avishka0303@gmail.com>
2019-04-21 00:59:45 +0530

    use incharge created

--- app/src/main/java/com/example/predatorx21/cebsmartmeter/dashboard/ControlFragment.java
+++ app/src/main/java/com/example/predatorx21/cebsmartmeter/dashboard/ControlFragment.java
@@ -2,14 +2,24 @@ package com.example.predatorx21.cebsmartmeter.dashboard;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import com.example.predatorx21.cebsmartmeter.R;
 
+import java.util.ArrayList;
+
 public class ControlFragment extends Fragment {
+
+    private Switch threshold_sw;
+    private Spinner typeList;
 
 
     public ControlFragment() {
@@ -20,4 +30,20 @@ public class ControlFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_control, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        threshold_sw=(Switch) getView().findViewById(R.id.threshold_switch);
+        typeList=(Spinner)getView().findViewById(R.id.type_list);
+        setupSpinnerTypeList();
+    }
+
+    private void setupSpinnerTypeList() {
+        ArrayList<String> list=new ArrayList<>();
+        list.add("Daily");
+        list.add("Monthly");
+        list.add("Custom");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeList.setAdapter(dataAdapter);
+    }
 }
---
+++ app/src/main/java/com/example/predatorx21/cebsmartmeter/utilities/ConsumptionCharge.java
@@ -0,0 +1,42 @@
+package com.example.predatorx21.cebsmartmeter.utilities;
+
+public class ConsumptionCharge {
+
+    private static final double BELOW60[] = {2.50, 4.85};
+    private static final double BELOW60FIX[] = {30, 60};
+
+    private static final double ABOVE60[] = {7.85, 10.00, 27.75, 32.00, 45.00};
+    private static final double ABOVE60FIX[] = {0, 90, 480, 480, 540};
+
+    private static double charge[];
+
+    public static double[] UsageInCharge(double unitskwh) {
+
+        if (unitskwh <= 30) {
+            charge[0] = unitskwh * BELOW60[0];
+            charge[1] = BELOW60FIX[0];
+            charge[3] = charge[0] + charge[1];
+        } else if (unitskwh <= 60) {
+            charge[0] = 30 * BELOW60[0] + (unitskwh-30) * BELOW60[1];
+            charge[1] = BELOW60FIX[1];
+            charge[3] = charge[0] + charge[1];
+        } else if (unitskwh <= 90) {
+            charge[0] = 60 * ABOVE60[0] + (unitskwh - 60) * ABOVE60[1];
+            charge[1] = ABOVE60FIX[2];
+            charge[3] = charge[0] + charge[1];
+        } else if (unitskwh <= 120) {
+            charge[0] = 60 * ABOVE60[0] + 30 * ABOVE60[1] + (unitskwh - 90) * ABOVE60[2];
+            charge[1] = ABOVE60FIX[3];
+            charge[3] = charge[0] + charge[1];
+        } else if (unitskwh <= 180) {
+            charge[0] = 60 * ABOVE60[0] + 30 * ABOVE60[1] + 30 * ABOVE60[2] + (unitskwh - 120) * ABOVE60[3];
+            charge[1] = ABOVE60FIX[4];
+            charge[3] = charge[0] + charge[1];
+        } else if (unitskwh >180) {
+            charge[0] = 60 * ABOVE60[0] + 30 * ABOVE60[1] + 30 * ABOVE60[2] + 30 * ABOVE60[3]+ (unitskwh - 180) * ABOVE60[4];
+            charge[1] = ABOVE60FIX[4];
+            charge[3] = charge[0] + charge[1];
+        }
+        return charge;
+    }
+}
